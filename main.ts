2c07cda48c3cac37dfbf74ed67c04ebc0211d5a2
tzafonetev3 <mos@tzafonet.org.il>
2019-08-26 18:26:58 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ load dependency
 "Tinybit": "file:../pxt-Tinybit"
 */
 
-//% color="#006400" weight=20 icon="\uf1b9"
+//% color="#02a7b0" weight=20 icon="\uf1b9"
 namespace Tinybit {
 
     const PWM_ADD = 0x01
