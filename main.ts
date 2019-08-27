e7b07e82abda93ef95f51bd08262ecb52fd94cf4
tzafonetev3 <mos@tzafonet.org.il>
2019-08-27 17:02:01 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -291,7 +291,7 @@ namespace Tinybit {
     //% weight=92
     //% blockGap=10
     //% speed.min=0 speed.max=255
-    //% color="#fc0328"
+    //% color="#f47e2d"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function CarCtrlSpeed(index: CarState, speed: number): void {
         switch (index) {
