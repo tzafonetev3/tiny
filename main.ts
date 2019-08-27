95dd2fe02b947d1ec30a35816563dc168fda5f3b
tzafonetev3 <mos@tzafonet.org.il>
2019-08-27 18:34:25 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -287,7 +287,7 @@ namespace Tinybit {
         }
     }
     
-    //% blockId=Tinybit_CarCtrlSpeed block="%speed במהירות   הפעלת רובוט |%index|"
+    //% blockId=Tinybit_CarCtrlSpeed block="במהירות %speed    הפעלת רובוט |%index|"
     //% weight=92
     //% blockGap=10
     //% speed.min=0 speed.max=255
