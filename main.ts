09bcce31becbf8dbd469b1fddfee42b7037649a7
tzafonetev3 <mos@tzafonet.org.il>
2019-08-27 22:45:32 +0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,400 @@
+﻿/*
+Copyright (C): 2010-2019, Shenzhen Yahboom Tech
+modified from liusen
+load dependency
+"Tinybit": "file:../pxt-Tinybit"
+*/
+
+//% color="#02a7b0" weight=20 icon="\uf1b9"
+namespace Tinybit {
+
+    const PWM_ADD = 0x01
+    const MOTOR = 0x02
+    const RGB = 0x01
+    
+    let yahStrip: neopixel.Strip;
+
+    export enum enColor {
+
+        //% blockId="OFF" block="כיבוי"
+        OFF = 0,
+        //% blockId="Red" block="אדום"
+        Red,
+        //% blockId="Green" block="ירוק"
+        Green,
+        //% blockId="Blue" block="כחול"
+        Blue,
+        //% blockId="White" block="לבן"
+        White,
+        //% blockId="Cyan" block="ציאן"רכב
+        Cyan,
+        //% blockId="Pinkish" block="ורדרד"
+        Pinkish,
+        //% blockId="Yellow" block="צהוב"
+        Yellow,
+
+    }
+    export enum enMusic {
+
+        dadadum = 0,
+        entertainer,
+        prelude,
+        ode,
+        nyan,
+        ringtone,
+        funk,
+        blues,
+
+        birthday,
+        wedding,
+        funereal,
+        punchline,
+        baddy,
+        chase,
+        ba_ding,
+        wawawawaa,
+        jump_up,
+        jump_down,
+        power_up,
+        power_down
+    }
+    export enum enPos {
+
+        //% blockId="LeftState" block="LeftState"
+        LeftState = 0,
+        //% blockId="RightState" block="RightState"
+        RightState = 1
+    }
+
+    export enum enLineState {
+        //% blockId="White" block="קו לבן"
+        White = 0,
+        //% blockId="Black" block="קו שחור"
+        Black = 1
+    }
+    
+    export enum enTouchState {
+        //% blockId="Get" block="Get"
+        Get = 0,
+        //% blockId="NoGet" block="NoGet"
+        NoGet = 1
+    }    
+    export enum enAvoidState {
+        //% blockId="OBSTACLE" block="מכשול"
+        OBSTACLE = 1,
+        //% blockId="NOOBSTACLE" block="אין מכשול"
+        NOOBSTACLE = 0
+
+    }
+    
+    export enum CarState {
+        //% blockId="Car_Run" block="הרץ"
+        Car_Run = 1,
+        //% blockId="Car_Back" block="אחורה"
+        Car_Back = 2,
+        //% blockId="Car_Left" block="שמאלה"
+        Car_Left = 3,
+        //% blockId="Car_Right" block="ימינה"
+        Car_Right = 4,
+        //% blockId="Car_Stop" block="עצור"
+        Car_Stop = 5,
+        //% blockId="Car_SpinLeft" block="סיבוב שמאלה"
+        Car_SpinLeft = 6,
+        //% blockId="Car_SpinRight" block="סיבוב ימינה"
+        Car_SpinRight = 7
+    }
+
+    function setPwmRGB(red: number, green: number, blue: number): void {
+
+        let buf = pins.createBuffer(4);
+        buf[0] = RGB;
+        buf[1] = red;
+        buf[2] = green;
+        buf[3] = blue;
+        
+        pins.i2cWriteBuffer(PWM_ADD, buf);
+    }
+
+    function setPwmMotor(mode: number, speed1: number, speed2: number): void {
+        if (mode < 0 || mode > 6)
+            return;
+        
+        let buf = pins.createBuffer(5);
+        buf[0] = MOTOR;
+        switch (mode) { 
+            case 0: buf[1] = 0; buf[2] = 0; buf[3] = 0; buf[4] = 0; break;              //stop
+            case 1: buf[1] = speed1; buf[2] = 0; buf[3] = speed2; buf[4] = 0; break;    //run
+            case 2: buf[1] = 0; buf[2] = speed1; buf[3] = 0; buf[4] = speed2; break;    //back
+            case 3: buf[1] = 0; buf[2] = 0; buf[3] = speed2; buf[4] = 0; break;         //left
+            case 4: buf[1] = speed1; buf[2] = 0; buf[3] = 0; buf[4] = 0; break;         //right
+            case 5: buf[1] = 0; buf[2] = speed1; buf[3] = speed2; buf[4] = 0; break;    //tleft
+            case 6: buf[1] = speed1; buf[2] = 0; buf[3] = 0; buf[4] = speed2; break;    //tright
+        }
+        pins.i2cWriteBuffer(PWM_ADD, buf);
+    }
+
+    function Car_run(speed1: number, speed2: number) {
+
+
+        setPwmMotor(1, speed1, speed2);
+    }
+
+    function Car_back(speed1: number, speed2: number) {
+
+        setPwmMotor(2, speed1, speed2);
+    }
+
+    function Car_left(speed1: number, speed2: number) {
+
+        setPwmMotor(3, speed1, speed2);
+    }
+
+    function Car_right(speed1: number, speed2: number) {
+
+        setPwmMotor(4, speed1, speed2);
+    }
+
+    function Car_stop() {
+       
+        setPwmMotor(0, 0, 0);
+    }
+
+    function Car_spinleft(speed1: number, speed2: number) {
+
+        setPwmMotor(5, speed1, speed2);
+    } 
+
+    function Car_spinright(speed1: number, speed2: number) {
+
+        setPwmMotor(6, speed1, speed2);
+    }
+
+    /**
+     * *****************************************************************
+     * @param index
+     */   
+
+    //% blockId=Tinybit_RGB_Car_Program block="RGB_Car_Program"
+    //% weight=99
+    //% blockGap=10
+    //% color="#02a7b0"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function RGB_Car_Program(): neopixel.Strip {
+         
+        if (!yahStrip) {
+            yahStrip = neopixel.create(DigitalPin.P12, 2, NeoPixelMode.RGB);
+        }
+        return yahStrip;  
+    }  
+
+    //% blockId=Tinybit_RGB_Car_Big block="RGB_Car_Big|value %value"
+    //% weight=98
+    //% blockGap=10
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function RGB_Car_Big(value: enColor): void {
+
+        switch (value) {
+            case enColor.OFF: {
+                setPwmRGB(0, 0, 0);
+                break;
+            }
+            case enColor.Red: {
+                setPwmRGB(255, 0, 0);
+                break;
+            }
+            case enColor.Green: {
+                setPwmRGB(0, 255, 0);
+                break;
+            }
+            case enColor.Blue: {
+                setPwmRGB(0, 0, 255);
+                break;
+            }
+            case enColor.White: {
+                setPwmRGB(255, 255, 255);
+                break;
+            }
+            case enColor.Cyan: {
+                setPwmRGB(0, 255, 255);
+                break;
+            }
+            case enColor.Pinkish: {
+                setPwmRGB(255, 0, 255);
+                break;
+            }
+            case enColor.Yellow: {
+                setPwmRGB(255, 255, 0);
+                break;
+            }
+        }
+    }
+    //% blockId=Tinybit_RGB_Car_Big2 block="RGB_Car_Big2|value1 %value1|value2 %value2|value3 %value3"
+    //% weight=97
+    //% blockGap=10
+    //% value1.min=0 value1.max=255 value2.min=0 value2.max=255 value3.min=0 value3.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function RGB_Car_Big2(value1: number, value2: number, value3: number): void {
+
+        setPwmRGB(value1, value2, value3);
+
+    }
+    //% blockId=Tinybit_Music_Car block="|%index|  רובוט מנגן"
+    //% weight=95
+    //% blockGap=10
+    //% color="#02a7b0"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function Music_Car(index: enMusic): void {
+        switch (index) {
+            case enMusic.dadadum: music.beginMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once); break;
+            case enMusic.birthday: music.beginMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once); break;
+            case enMusic.entertainer: music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once); break;
+            case enMusic.prelude: music.beginMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once); break;
+            case enMusic.ode: music.beginMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once); break;
+            case enMusic.nyan: music.beginMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once); break;
+            case enMusic.ringtone: music.beginMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once); break;
+            case enMusic.funk: music.beginMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once); break;
+            case enMusic.blues: music.beginMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once); break;
+            case enMusic.wedding: music.beginMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Once); break;
+            case enMusic.funereal: music.beginMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once); break;
+            case enMusic.punchline: music.beginMelody(music.builtInMelody(Melodies.Punchline), MelodyOptions.Once); break;
+            case enMusic.baddy: music.beginMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once); break;
+            case enMusic.chase: music.beginMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once); break;
+            case enMusic.ba_ding: music.beginMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once); break;
+            case enMusic.wawawawaa: music.beginMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once); break;
+            case enMusic.jump_up: music.beginMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once); break;
+            case enMusic.jump_down: music.beginMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once); break;
+            case enMusic.power_up: music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once); break;
+            case enMusic.power_down: music.beginMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once); break;
+        }
+    }
+    
+    
+    
+    //% blockId=Tinybit_CarCtrl block="|%index| הפעלת רובוט"
+    //% weight=93
+    //% blockGap=10
+    //% color="#666666"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrl(index: CarState): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(255, 255); break;
+            case CarState.Car_Back: Car_back(255, 255); break;
+            case CarState.Car_Left: Car_left(255, 255); break;
+            case CarState.Car_Right: Car_right(255, 255); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(255, 255); break;
+            case CarState.Car_SpinRight: Car_spinright(255, 255); break;
+        }
+    }
+    
+    //% blockId=Tinybit_CarCtrlSpeed block="במהירות %speed    הפעלת רובוט |%index|"
+    //% weight=92
+    //% blockGap=10
+    //% speed.min=0 speed.max=255
+    //% color="#8c080a"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrlSpeed(index: CarState, speed: number): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(speed, speed); break;
+            case CarState.Car_Back: Car_back(speed, speed); break;
+            case CarState.Car_Left: Car_left(speed, speed); break;
+            case CarState.Car_Right: Car_right(speed, speed); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(speed, speed); break;
+            case CarState.Car_SpinRight: Car_spinright(speed, speed); break;
+        }
+    }
+    
+    //% blockId=Tinybit_CarCtrlSpeed2 block="הפעלת גלגלי הרובוט במהירות שונות |%index|speed1 %speed1|speed2 %speed2"
+    //% weight=91
+    //% blockGap=10
+    //% speed1.min=0 speed1.max=255 speed2.min=0 speed2.max=255
+    //% color="#02a7b0"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrlSpeed2(index: CarState, speed1: number, speed2: number): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(speed1, speed2); break;
+            case CarState.Car_Back: Car_back(speed1, speed2); break;
+            case CarState.Car_Left: Car_left(speed1, speed2); break;
+            case CarState.Car_Right: Car_right(speed1, speed2); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(speed1, speed2); break;
+            case CarState.Car_SpinRight: Car_spinright(speed1, speed2); break;
+        }
+    }    
+        
+   
+    
+    //% blockId=Tinybit_Line_Sensor block="חיישן קו|direct %direct|ערך %value"    //% weight=89
+    //% blockGap=10
+    //% color="#1e4b7a"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
+    export function Line_Sensor(direct: enPos, value: enLineState): boolean {
+
+        let temp: boolean = false;
+        pins.setPull(DigitalPin.P13, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
+        switch (direct) {
+            case enPos.LeftState: {
+                if (pins.digitalReadPin(DigitalPin.P13) == value) {              
+                    temp = true;                  
+                }
+                else {                  
+                     temp = false;
+                }
+                break;
+            }
+
+            case enPos.RightState: {
+                if (pins.digitalReadPin(DigitalPin.P14) == value) {              
+                    temp = true;                  
+                }
+                else {
+                    temp = false;
+                }
+                break;
+            }
+        }
+        return temp;
+
+    }
+
+    //% blockId=Tinybit_Voice_Sensor block="ערך חיישן קול"
+    //% weight=88
+    //% blockGap=10
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
+    export function Voice_Sensor(): number {
+	    //pins.setPull(DigitalPin.P1, PinPullMode.PullUp);
+        let temp  = 0;		
+        temp = pins.analogReadPin(AnalogPin.P1);           
+            
+        return temp;
+
+    }
+        
+    //% blockId=Tinybit_Ultrasonic_Car block="ערך חיישן אולטראסוניק בסנטימטר"
+    //% color="#02a7b0"
+    //% weight=87
+    //% blockGap=10
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function Ultrasonic_Car(): number {
+
+       let list:Array<number> = [0, 0, 0, 0, 0];
+	for (let i = 0; i < 5; i++)
+	{
+		pins.setPull(DigitalPin.P16, PinPullMode.PullNone);
+		pins.digitalWritePin(DigitalPin.P16, 0);
+		control.waitMicros(2);
+		pins.digitalWritePin(DigitalPin.P16, 1);
+		control.waitMicros(15);
+		pins.digitalWritePin(DigitalPin.P16, 0);
+		let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 43200);
+		list[i] = Math.floor(d / 40);
+	}
+	list.sort();
+	let length = (list[1] + list[2] + list[3])/3;
+	return  Math.floor(length);
+    }
+
+
+}
