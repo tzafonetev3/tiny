c32912dd50f289c1e4bb323560deba3eb336f057
tzafonetev3 <mos@tzafonet.org.il>
2019-08-28 19:19:29 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -273,7 +273,7 @@ namespace Tinybit {
     //% blockId=Tinybit_CarCtrl block="|%index| הפעלת רובוט"
     //% weight=93
     //% blockGap=10
-    //% color="#666666"
+    //% color="#d7761b"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
     export function CarCtrl(index: CarState): void {
         switch (index) {
